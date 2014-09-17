b16de438db23638114a077a6defae99b8205d02b
baoxue <baoxue.zhao@gmail.com>
2014-09-17 11:30:31 +0800

    Update PrimeListGenerating.cpp

--- chapter7/PrimeListGenerating.cpp
+++ chapter7/PrimeListGenerating.cpp
@@ -1,4 +1,23 @@
 void primeListGenerating(int n)
 {
-	bool 
+	bool isPrime[n+1];
+	
+	for(int i=2; i<n+1; i++)
+		isPrime[i] = true;
+	
+	int prime = 2;
+	while(prime <= sqrt(n))
+	{
+		for(int cur = prime*prime; cur < n+1; cur += prime)
+			isPrime[cur] = false;
+		
+		for(int cur = prime+1; cur < n+1; cur++)
+		{
+			if(isPrime[cur])
+			{
+				prime = cur;
+				break;
+			}
+		}
+	}
 }
