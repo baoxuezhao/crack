9e334539fce023642ea6fa1ae1b9cd2dbab527a3
baoxue <baoxue.zhao@gmail.com>
2014-09-22 22:28:02 +0800

    Create Q10.7.cpp

---
+++ chapter10/Q10.7.cpp
@@ -0,0 +1,6 @@
+Step 1: design cache for single machine.
+Step 2: extend to many machine.
+  opt 1. each machine has its own cache
+  opt 2. each machine stores a segment of the whole cache. segment is hashed by  hash(query)%N
+
+Step 3: updating cache result.
