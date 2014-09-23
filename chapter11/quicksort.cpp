f416781e2fd65c2dc2d6c26914d53090a1de508c
baoxue <baoxue.zhao@gmail.com>
2014-09-23 12:01:38 +0800

    Update quicksort.cpp

--- chapter11/quicksort.cpp
+++ chapter11/quicksort.cpp
@@ -3,7 +3,8 @@
 
 using namespace std;
 
-int partition(int array[], int left, int right)
+//take the middle element as pivot
+int partition1(int array[], int left, int right)
 {
 	int pivot = array[(left+right)/2];
 	while(left <= right)
@@ -22,8 +23,40 @@ int partition(int array[], int left, int right)
 	return left;
 }
 
-void quicksort(int array[], int left, int right)
+void quicksort1(int array[], int left, int right)
 {
-	int index = partition(array, left, right);
+	int index = partition1(array, left, right);
+	if(left < index-1)
+		quicksort1(array, left, index-1);
+	if(index < right)
+		quicksort1(array, index, right);
+}
+
+
+///version 2. take the last element as pivot. taken from <<Introduction to Algorithms>>.
+//take the middle element as pivot
+int partition2(int array[], int left, int right)
+{
+	int pivot = array[right];
+	int i = left-1;
+	for(int j=left; j<=right; j++)
+	{
+		if(array[j] <= pivot)
+		{
+			i++;
+			swap(array[i], array[j]);
+		}
+	}
 	
+	return i;
+}
+
+void quicksort2(int array[], int left, int right)
+{
+	if(left < right)
+	{
+		int index = partition2(array, left, right);
+		quicksort2(array, left, index-1);
+		quicksort2(array, index+1, right);
+	}
 }
