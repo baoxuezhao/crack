2e8db3907efb31a8304e17d5fef00fbf09006901
baoxue <baoxue.zhao@gmail.com>
2014-09-25 16:21:37 +0800

    Update Q17.14.cpp

--- chapter17/Q17.14.cpp
+++ chapter17/Q17.14.cpp
@@ -1,32 +1,64 @@
 //target, min number of unrecognized characters.
-
 #include <iostream>
 #include <vector>
 #include <unordered_set>
+using namespace std;
 
-int minUnrecoginized(string str, unordered_set<string> dict)
+//target, min number of unrecognized characters.
+vector<int> minUnrecoginized(string str, unordered_set<string> dict)
 {
 	const int len = str.size();
-	
+
 	vector<vector<bool> > contains(len, vector<bool>(len, false));
-	vector<int> f(len, 0);
-	
+	vector<int> f(len+1, 0);
+	vector<int> prev(len, -1);
+
 	for(int i=0; i<len; i++)
 	{
 		for(int j=i; j<len; j++)
 		{
-			contains[i][j] = dict.contains(str.substr(i, j-i+1));
+			contains[i][j] = dict.count(str.substr(i, j-i+1))>0;
 		}
 	}
-	
+
+	prev[0] = -1;
+
 	for(int i=0; i<len; i++)
 	{
-		f[i] = contains[0][i]?0:(i+1);
-		for(int j=0; j<i; j++)
+		f[i+1] = (contains[0][i]?0:(i+1));
+		for(int j=-1; j<i; j++)
 		{
-			int tmp = f[j] + contains[j+1][i]? 0:(i-j);
-			f[i] = min(f[i], tmp);
+			int tmp = f[j+1] + ((contains[j+1][i]==true)? 0:(i-j));
+			if(tmp < f[i+1])
+			{
+				f[i+1] = tmp;
+				prev[i] = j;
+			}
 		}
 	}
-	return f[len-1];
+	cout << f[len] << endl;
+	return prev;
+}
+
+int main()
+{
+	string s = "jesslookedjustliketimherbrother";
+
+	unordered_set<string> dict;
+	dict.insert("looked");
+	dict.insert("just");
+	dict.insert("like");
+	dict.insert("her");
+	dict.insert("brother");
+
+	vector<int> prev = minUnrecoginized(s, dict);
+	int end = s.length()-1;
+
+	while(end >= 0)
+	{
+		cout << s.substr(prev[end]+1, end-prev[end]) << endl;
+		end = prev[end];
+	}
+
+	return 0;
 }
